23ee0b89030ada78b41fc5d6d42c69385bd23593
vaishnavijaiswal <vaishnavijaiswal3@gmail.com>
2026-09-01 12:13:10 +0530

    Add dark mode feature

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function showMessage() {
 function changeTitle() {
     document.querySelector("h1").innerText = "Welcome to My Portfolio";
 }
+
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeBtn.innerText = "☀️ Light Mode";
+    } else {
+        themeBtn.innerText = "🌙 Dark Mode";
+    }
+});
